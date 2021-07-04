90c1217a8575a67df339b2370c6497fad4715cf2
aradzimski <adrianradzimski@gmail.com>
2021-07-04 18:35:56 +0200

    Added repository dependency, created forum links

--- src/Controller/ForumController.php
+++ src/Controller/ForumController.php
@@ -5,11 +5,21 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\TopicRepository;
 
 class ForumController extends AbstractController
 {
-    #[Route('/forum', name: 'forum')]
-    public function index(): Response
+    private TopicRepository $topicRepository;
+
+    public function __construct(TopicRepository $topicRepository)
+    {
+        $this->topicRepository = $topicRepository;
+    }
+
+    /**
+     * @Route("/", name="app_forum")
+     */
+    public function index(int $id): Response
     {
         return $this->render('forum/index.html.twig', [
             'controller_name' => 'ForumController',
