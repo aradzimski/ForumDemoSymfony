d9c5e6128f1de7668196cb6a585bd99741b74be5
aradzimski <adrianradzimski@gmail.com>
2021-07-04 15:42:27 +0200

    Forums listing on home page

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,16 +5,27 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\ForumRepository;
 
 class HomeController extends AbstractController
 {
+    private ForumRepository $forumRepository;
+
+    public function __construct(ForumRepository $forumRepository)
+    {
+        $this->forumRepository = $forumRepository;
+    }
+
     /**
      * @Route("/", name="app_home")
      */
     public function index(): Response
     {
+        $forums = $this->forumRepository->findAll();
+
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+            'forums' => $forums,
         ]);
     }
 }
