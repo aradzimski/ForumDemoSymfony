755cfa6374af50700f5c518a00ac4f6dc8af1af1
aradzimski <adrianradzimski@gmail.com>
2021-07-05 16:15:25 +0200

    Throw exception if forum is not found

--- src/Controller/ForumController.php
+++ src/Controller/ForumController.php
@@ -4,12 +4,14 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Forum;
 use App\Repository\ForumRepository;
 
 class ForumController extends AbstractController
 {
-    private ForumRepository $forumRepository;
+private ForumRepository $forumRepository;
 
     public function __construct(ForumRepository $forumRepository)
     {
@@ -22,6 +24,10 @@ class ForumController extends AbstractController
     public function index(int $id): Response
     {
         $forum = $this->forumRepository->find($id);
+
+        if (!$forum instanceof Forum) {
+            throw new NotFoundHttpException();
+        }
 
         return $this->render('forum/index.html.twig', [
             'controller_name' => 'ForumController',
