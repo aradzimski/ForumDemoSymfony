a5c3cc1a6b7ed58d1c101caa9ae12a7e26f7cda8
aradzimski <adrianradzimski@gmail.com>
2021-07-05 13:40:10 +0200

    Listing posts in topic

--- src/Controller/TopicController.php
+++ src/Controller/TopicController.php
@@ -5,14 +5,27 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\TopicRepository;
 
 class TopicController extends AbstractController
 {
-    #[Route('/topic', name: 'topic')]
-    public function index(): Response
+    private TopicRepository $topicRepository;
+
+    public function __construct(TopicRepository $topicRepository)
     {
+        $this->topicRepository = $topicRepository;
+    }
+
+    /**
+     * @Route("/topic/{id}/{urltitle}", name="app_topic")
+     */
+    public function index(int $id): Response
+    {
+        $topic = $this->topicRepository->find($id);
+
         return $this->render('topic/index.html.twig', [
             'controller_name' => 'TopicController',
+            'topic' => $topic,
         ]);
     }
 }
